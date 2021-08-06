8e741304415625d0db0678a544a5e8b507f00687
lwalter <lwalter1022@hotmail.com>
2021-08-06 15:51:38 -0700

    revist for new install

--- .zshrc
+++ .zshrc
@@ -52,3 +52,6 @@ fi
 if [ -f /usr/bin/direnv ]; then
 	eval "$(direnv hook zsh)";
 fi
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
--- install.sh
+++ install.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-# install zsh, i3, and vundle
-#sudo apt-get install zsh i3wm feh arc-theme lxappearance papirus-icon-theme light redshift i3blocks cmake build-essential python-dev python3-dev
+sudo apt-get install zsh i3 feh lxappearance i3blocks cmake build-essential python-dev python3-dev tmux curl
 
-#git clone git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# JS, CSS, HTML, i3statusbar
+
+# install go
+curl https://golang.org/dl/go1.16.7.linux-amd64.tar.gz --output go1.16.7.linux-amd64.tar.gz
+rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf go1.16.7.linux-amd64.tar.gz
+
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files=".tmux.conf .vimrc .zshrc .i3/config .i3/i3blocks.conf wallpaper.jpg .gtkrc-2.0 .config/gtk-3.0/settings.ini .config/gtk-4.0/settings.ini"
-
-echo "INSTALL ARK-DARKER FOR CHROME MANULLLY"
+files=".tmux.conf .vimrc .zshrc .config/i3/config .config/i3/i3blocks.conf wallpaper.jpg"
 
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
@@ -28,3 +33,25 @@ for file in $files; do
     ln -sf $dir/$file ~/$file
 done
 
+source ~/.zshrc
+nvm install node
+
+# cd ~/.vim/bundle/YouCompleteMe
+# python3 install.py --all
+
+# install docker
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
