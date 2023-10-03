2178c5605a9c8d95c318aaf986352528689721f7
lwalter <lwalter1022@hotmail.com>
2023-10-02 19:00:15 -0700

    Fix golang auto imports

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -245,25 +245,23 @@ local on_attach = function(_, bufnr)
         end,
     })
 
-    local filetype = vim.filetype.match({ buf = bufnr })
-    if filetype == "go" then
-        local params = vim.lsp.util.make_range_params()
-        params.context = { only = { "source.organizeImports" } }
-        local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
-        for _, res in pairs(result or {}) do
-            for _, r in pairs(res.result or {}) do
-                if r.edit then
-                    vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
-                else
-                    vim.lsp.buf.execute_command(r.command)
-                end
-            end
-        end
-    end
-
     -- Always attempt to format on save from LSP
     vim.api.nvim_create_autocmd("BufWritePre", {
         callback = function()
+            local filetype = vim.filetype.match({ buf = bufnr })
+            if filetype == "go" then
+                local params = vim.lsp.util.make_range_params()
+                params.context = { only = { "source.organizeImports" } }
+                local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+                for cid, res in pairs(result or {}) do
+                    for _, r in pairs(res.result or {}) do
+                        if r.edit then
+                            local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+                            vim.lsp.util.apply_workspace_edit(r.edit, enc)
+                        end
+                    end
+                end
+            end
             vim.lsp.buf.format({ async = false, bufnr = bufnr, timeout_ms = 3000 })
         end,
         group = format_grp,
